88a978c5bcf7f7ee0ae0f3f327bba161c279eadc
anasolomon <anamariatanasciuc@gmail.com>
2023-07-23 19:13:47 -0500

    added compose page

--- app.js
+++ app.js
@@ -25,6 +25,13 @@ app.get("/about", function(req, res){
 app.get("/contact", function(req, res){
   res.render("contact", {contactContent: contactStartingContent});
 });
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+app.post("/compose", function(req,res){
+  const newComposed = req.body.newCompose;
+  console.log(newComposed);
+});
 
 
 
