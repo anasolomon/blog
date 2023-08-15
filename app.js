10adb0b4b72ac7f9192ed84fab55585c386b0d31
Ana-Maria Solomon <anamariatanasciuc@gmail.com>
2023-08-15 16:01:50 -0500

    added database

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require('mongoose');
+const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutStartingContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -10,36 +12,57 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+mongoose.connect('mongodb+srv://admin-ana:test123@cluster0.anmrdwa.mongodb.net/blogDB', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  body: String
+});
+const Blog = mongoose.model("Blog", blogSchema);
 const postArray = [];
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
 
-  res.render("home", {homeContent: homeStartingContent, htmlArray: postArray});
+  Blog.find({}, function (err, blogsFound) {
+    if (!err) {
+      res.render("home", { homeContent: homeStartingContent, htmlArray: blogsFound });
+    }
+  });
+
 });
 
 
-app.get("/about", function(req, res){
-  res.render("about", {aboutContent: aboutStartingContent});
+app.get("/about", function (req, res) {
+  res.render("about", { aboutContent: aboutStartingContent });
 });
 
 
-app.get("/contact", function(req, res){
-  res.render("contact", {contactContent: contactStartingContent});
+app.get("/contact", function (req, res) {
+  res.render("contact", { contactContent: contactStartingContent });
 });
 
 
 
-app.get("/compose", function(req, res){
+app.get("/compose", function (req, res) {
   res.render("compose");
 });
 
-app.post("/compose", function(req,res){
+app.post("/compose", function (req, res) {
+  const blogTitle = req.body.postTitle;
+  const blogBody = req.body.postBody;
+
+  const blog = new Blog({
+    title: blogTitle,
+    body: blogBody
+  });
+  blog.save();
+
   const post = {
-    title: req.body.postTitle,
-    body: req.body.postBody
+    title: blogTitle,
+    body: blogBody
   };
 
   postArray.push(post);
@@ -47,17 +70,16 @@ app.post("/compose", function(req,res){
 });
 
 
-app.get("/posts/:postName", function(req, res){
-  for (let i = 0; i < postArray.length; i++) {
-  if(req.params.postName == postArray[i].title){
-    res.render("posts", {storedTitle: postArray[i].title, storedBody: postArray[i].body});
-   };
- };
+app.get("/posts/:postNameId", function (req, res) {
+  const requestedBlogId = req.params.postNameId;
+  Blog.findOne({ _id: requestedBlogId }, function (err, foundBlogName) {
+    res.render("posts", { storedTitle: foundBlogName.title, storedBody: foundBlogName.body });
+  });
 });
 
 
 
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
--- readme.md
+++ readme.md
@@ -1,9 +1,11 @@
 [Process](#Process)  
+ - [Connecting to a Database](#connecting-to-a-database)    
+
 [Learned](#Learned)  
 [Screenshots](#screenshots)
 
 
-### Process
+# Process
 I've added the modules :
 ```js
 const express = require("express");
@@ -71,6 +73,60 @@ Inside of the posts file I will use EJS to display the blog's title in the pathi
  <% } %>
 ```
 
+## Connecting to a Database
+
+Declaring a Schema and model for the Table where I will store the blog posts. 
+```js
+const blogSchema = new mongoose.Schema({
+  title: String,
+  body: String
+});
+const Blog = mongoose.model("Blog", blogSchema);
+```
+
+Whenever the user goes to the /compose url page and clicks the Submit button then this code will execute and save all data entered in the form inputs into the new table as Documents :
+```js
+app.post("/compose", function(req,res){
+  const blogTitle = req.body.postTitle;
+  const blogBody = req.body.postBody;
+
+  const blog = new Blog({
+    title: blogTitle,
+    body: blogBody
+  });
+  blog.save();
+});
+```
+
+To display that data in the home page I've added this block of code to the root route app.get : 
+```js
+app.get("/", function (req, res) {
+
+  Blog.find({}, function (err, blogsFound) {
+    if (!err) {
+      res.render("home", { homeContent: homeStartingContent, htmlArray: blogsFound });
+    }
+  });
+
+});
+```
+In the code above whenever the user is redirected or goes to the root route then the code will :
+ - Execute the `find()` function which basically displays all of the Documents inside of the "Blog" table by passing them as an object to the `blogsFound` variable
+ - If no errors found then *home.ejs* will get rendered and `blogsFound` will be displayed in the html page as `htmlArray`
+
+For the dynamic route to also work I updated the old code :
+```js
+app.get("/posts/:postNameId", function (req, res) {
+  const requestedBlogId = req.params.postNameId;
+  Blog.findOne({ _id: requestedBlogId }, function (err, foundBlogName) {
+    res.render("posts", { storedTitle: foundBlogName.title, storedBody: foundBlogName.body });
+  });
+});
+```
+In the code above :
+ - We take from the home.ejs form `<a class="test" href='/posts/<%=currentPost._id%>'>Read More</a>` the id of a document use clicked "read more" on and we paste it at end of URL. This request can happened because we have an app.get that accepts dynamic routes.
+ - We are checking if the request for dynamic route from user corrisponds to an existing document's id. If yes then we render the posts.ejs page with storedTitle = document.title and storedBody = document.body
+
 
 
  ### Learned
@@ -114,12 +170,11 @@ If you are using EJS to display a text then you can integrate a little bit of Ja
  <% } %>
 ```
 
+You can pass in a dynamic route not just manually by writting it in the URL by hand but also by redirecting towards it with a href and a dynamic value as the link `app.get("/posts/:postNameId",` `<a class="test" href='/posts/<%=currentPost._id%>'>Read More</a>`
+
 ### Screenshots
 
-`localhost:3000`
-![](https://media.discordapp.net/attachments/1039676646325764231/1133078944439079052/Screenshot_2023-07-24_at_11-49-12_Daily_Journal.png?width=1346&height=753) 
-After we've composed a blog post in `localhost:3000/compose`
-![](https://media.discordapp.net/attachments/1039676646325764231/1133078944191619103/Screenshot_2023-07-24_at_11-49-46_Daily_Journal.png?width=1346&height=753) 
-We access that blog post in 
-`localhost:3000/posts/Test`
-![](https://media.discordapp.net/attachments/1039676646325764231/1133078944699129906/Screenshot_2023-07-24_at_11-49-56_Daily_Journal.png?width=1346&height=753) 
+Main page
+![](https://media.discordapp.net/attachments/1141016274160328756/1141113779984269392/Screenshot_2023-08-15_at_15-57-51_Daily_Journal.png?width=1360&height=676) 
+Composing a blog post in `/compose`
+![](https://media.discordapp.net/attachments/1141016274160328756/1141113779380301885/Screenshot_2023-08-15_at_15-58-12_Daily_Journal.png?width=1360&height=676) 
